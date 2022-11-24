bc611789dffc853227602456a8272e7ad7cce03d
guiscoz <gui.x.scoz@gmail.com>
2022-11-24 09:36:50 -0300

    atualização no perfil

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,25 @@
 <template>
-    <div id="profile">
-        <h1>Bem-vindo, {{ name }}.</h1>
+    <div class="container">
+    <div id="dog-table" >
+      <h1>Bem vindo, {{ this.name }}</h1>
+      <h2 v-if="dogs">Veja os cachorros já cadastrados por você</h2>
+      <div id="dog-table-rows" v-if="dogs">
+        <div class="dog-table-row" v-for="dog in dogs" :key="dog.id" >
+          <div class="dog-table-column">
+            <img v-if="dog.img_path" :src="`http://127.0.0.1:8000/storage/${dog.img_path}`" :alt="dog.name" class="dog-picture">
+            <img v-else src="@/assets/imgs/noImage.jpg" :alt="dog.name" class="dog-picture">
+          </div>
+          <div class="dog-table-column">
+            <div class="dog-data">
+              <p><b>Nome:</b> {{dog.name}}</p>
+              <p><b>Raça:</b> {{dog.breed}}</p>
+              <p><b>Sexo:</b> {{dog.gender == "M" ? "Masculino" : "Feminino"}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -15,7 +33,8 @@
         data() {
             return {
                 name: null,
-                id: null
+                id: null,
+                dogs: null
             }
         },
         methods: {
@@ -26,9 +45,9 @@
                 }
                 })
                 .then(response => {
-                    this.name = response.data.name
-                    this.id = response.data.id
-                    console.log(response)
+                    this.name = response.data.user.name
+                    this.id = response.data.user.id
+                    this.dogs = response.data.dogs.data
                 })
                 .catch((error) => {
                     console.log(error)
@@ -40,3 +59,36 @@
         }
     }
 </script>
+
+<style scoped>
+  #dog-table {
+    max-width: 1200px;
+    margin: 3% auto 0;
+  }
+
+  .dog-table-row {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 450px;
+    margin-bottom: 3%;
+    border: 1px solid #333;
+  }
+
+  .dog-table-column {
+    margin: auto 0;
+  }
+
+  .dog-picture {
+    max-height: 400px;
+    max-width: 400px;
+  }
+
+  .dog-data {
+    font-size: 25px;
+  }
+
+  .dog-data b {
+    font-size: 20px;
+  }
+</style>
